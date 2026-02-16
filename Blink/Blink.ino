2bfba4b3a4ab094b7e2bf4fc32332043bb7fdd7a
sofiyakossara <amirova1527tll@gmail.com>
2026-02-17 00:15:37 +0300

    feat: add SOS signal function

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -1,10 +1,39 @@
 void setup() {
-  pinMode(12, OUTPUT); // Встроенный светодиод обычно на пине 13
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(12, HIGH);   // включить светодиод
-  delay(100);              // подождать 1 секунду
-  digitalWrite(12, LOW);    // выключить светодиод
-  delay(100);              // подождать 1 секунду
+  blinkSOS();  // режим SOS
+}
+
+void blinkSOS() {
+  // Три коротких (S)
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(13, HIGH);
+    delay(150);
+    digitalWrite(13, LOW);
+    delay(150);
+  }
+  
+  delay(300); // пауза между буквами
+  
+  // Три длинных (O)
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(13, HIGH);
+    delay(450);
+    digitalWrite(13, LOW);
+    delay(150);
+  }
+  
+  delay(300); // пауза между буквами
+  
+  // Три коротких (S)
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(13, HIGH);
+    delay(150);
+    digitalWrite(13, LOW);
+    delay(150);
+  }
+  
+  delay(2000); // пауза перед повтором
 }
